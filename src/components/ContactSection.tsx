9252d4cb0ee176b43e69f9b9703e7026445c99ef
Kapil <kapilguptakg1519@gmail.com>
2025-08-28 12:49:54 +0530

    backend connected

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -54,7 +54,7 @@ const ContactSection: React.FC = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://backend-ed-x2dh.vercel.app/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
